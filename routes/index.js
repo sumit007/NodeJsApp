d8164b97309fab1cf4ba2c65d3fbbbf7a375da14
sumit007 <sumit@nestaway.com>
2019-02-19 15:10:18 +0530

    show list of leads on submission

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,6 @@ let landing = require('../controller/landing');
 /* GET Home page */
 router.get('/', landing.get_landing);
 router.post('/', landing.submit_lead);
+router.get('/leads', landing.show_leads);
 
 module.exports = router;
