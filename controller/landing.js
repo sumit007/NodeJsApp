53d8604a4af10012c83d7273bd136bc19dd96612
sumit007 <sumit@nestaway.com>
2019-02-21 00:26:59 +0530

    chnaged all the view tousing bootstrap

--- controller/landing.js
+++ controller/landing.js
@@ -1,54 +1,49 @@
 const models = require('../models')
 
 exports.get_landing = function(req, res, next) {
-  res.render('landing', { title: 'Express'});
+  res.render('landing', { title: 'Express', user: req.user });
 }
 
 exports.submit_lead = function(req, res, next) {
-	//console.log("Lead email : ", req.body.lead_email);
 
 	return models.Lead.create({
-		email: req.body.lead_email,
+		email: req.body.lead_email
 	}).then(lead => {
 		res.redirect('/leads');
 	})
 }
 
 exports.show_leads = function(req, res, next) {
-	models.Lead.findAll().then(leads => {
-		res.render('landing', { title: 'Express', leads: leads});
+	return models.Lead.findAll().then(leads => {
+ 		res.render('lead/leads', { title: 'Express', leads: leads });		
 	})
-  
 }
 
 exports.show_lead = function(req, res, next) {
-
 	return models.Lead.findOne({
 		where : {
-			id: req.params.lead_id
+			id : req.params.lead_id
 		}
 	}).then(lead => {
-		res.render('lead', {lead: lead});
+		res.render('lead/lead', { lead : lead });
 	});
-  
 }
 
 exports.show_edit_lead = function(req, res, next) {
-
 	return models.Lead.findOne({
 		where : {
-			id: req.params.lead_id
+			id : req.params.lead_id
 		}
 	}).then(lead => {
-		res.render('lead/edit_lead', {lead: lead});
+		res.render('lead/edit_lead', { lead : lead });
 	});
-  
 }
 
 exports.edit_lead = function(req, res, next) {
+
 	return models.Lead.update({
 		email: req.body.lead_email
-	}, {
+	}, { 
 		where: {
 			id: req.params.lead_id
 		}
@@ -56,7 +51,6 @@ exports.edit_lead = function(req, res, next) {
 		res.redirect('/lead/' + req.params.lead_id);
 	})
 }
-
 exports.delete_lead = function(req, res, next) {
 	return models.Lead.destroy({
 		where: {
@@ -73,8 +67,6 @@ exports.delete_lead_json = function(req, res, next) {
 			id: req.params.lead_id
 		}
 	}).then(result => {
-		res.send({
-			msg: "Success"
-		});
+		res.send({ msg: "Success" });
 	})
 }
